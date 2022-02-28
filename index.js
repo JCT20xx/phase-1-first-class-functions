2a2dff5662a1db25690a99081589095fbe00b900
Juan Toro <jct20xx@gmail.com>
2022-02-27 22:38:30 -0500

    commit tree 78e66153032251366e971b263b5192dc2accc9d0

---
+++ index.js
@@ -0,0 +1,15 @@
+function receivesAFunction (callback) {
+    return callback();
+}
+
+function returnsANamedFunction(){
+    return function named() {
+        return ;
+    }
+}
+
+function returnsAnAnonymousFunction(){
+    return function () {
+        return ;
+    }
+}
